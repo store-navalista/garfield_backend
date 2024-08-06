b76fb2cd7bc2ff354d5f689ff30c561750f2a00a
Sergey Otinov <otinov.prog@gmail.com>
2024-08-06 15:11:08 +0300

    ssl

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 import { NestApplication, NestFactory } from '@nestjs/core'
-// import * as fs from 'fs-extra'
+import * as fs from 'fs-extra'
 // import helmet from 'helmet'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
@@ -8,26 +8,16 @@ import { AppModule } from './app.module'
 async function bootstrap() {
    const PORT = process.env.PORT || 8879
 
-   // const httpsOptions = {
-   //    key: fs.readFileSync('secret/key.pem'),
-   //    cert: fs.readFileSync('secret/cert.pem')
-   // }
+   const httpsOptions = {
+      key: fs.readFileSync(' /etc/letsencrypt/live/app.navalista.com/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/app.navalista.com/cert.pem')
+   }
 
    const graphqlUploadExpress = (await import('graphql-upload/graphqlUploadExpress.mjs')).default
 
-   //  const app = await NestFactory.create(AppModule, {httpsOptions});
-   const app = await NestFactory.create<NestApplication>(AppModule)
+   const app = await NestFactory.create(AppModule, { httpsOptions })
+   // const app = await NestFactory.create<NestApplication>(AppModule)
    app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }))
-
-   // const corsOptions: CorsOptions = {
-   //    origin: [
-   //      'https://www.keelwards.com',
-   //      'https://keelwards.com',
-   //    ],
-   //    credentials: true
-   //  };
-
-   //  app.enableCors(corsOptions);
 
    const corsOptions = {
       origin: ['https://www.app.navalista.com', 'https://app.navalista.com'],
