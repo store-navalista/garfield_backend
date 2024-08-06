0c716dc45e288b9b71a41af668d6d56e8577c817
Sergey Otinov <otinov.prog@gmail.com>
2024-08-06 19:53:53 +0300

    check

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,8 @@ async function bootstrap() {
 
    const graphqlUploadExpress = (await import('graphql-upload/graphqlUploadExpress.mjs')).default
 
-   const app = await NestFactory.create(AppModule, { httpsOptions })
-   // const app = await NestFactory.create<NestApplication>(AppModule)
+   // const app = await NestFactory.create(AppModule, { httpsOptions })
+   const app = await NestFactory.create<NestApplication>(AppModule)
    app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }))
 
    const corsOptions = {
@@ -26,20 +26,9 @@ async function bootstrap() {
       allowedHeaders: 'Content-Type, Accept, Authorization'
    }
 
-   // app.enableCors(corsOptions)
+   app.enableCors()
    app.use(cookieParser())
 
-   app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'https://www.app.navalista.com, https://app.navalista.com')
-      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS')
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization')
-      res.header('Access-Control-Allow-Credentials', 'true')
-      if (req.method === 'OPTIONS') {
-         return res.sendStatus(204)
-      }
-      next()
-   })
-
-   await app.listen(PORT).then(() => console.log(`App run on https://localhost:${PORT}`))
+   await app.listen(PORT).then(() => console.log(`App run on https://127.0.0.1:${PORT}`))
 }
 bootstrap()
