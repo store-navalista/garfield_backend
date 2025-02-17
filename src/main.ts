58e8d38f678e5cd0410d69f7f19317bf9a375c03
Sergey Otinov <otinov.prog@gmail.com>
2025-02-17 09:25:33 +0200

    https

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestApplication, NestFactory } from '@nestjs/core'
-import * as fs from 'fs-extra'
+// import * as fs from 'fs-extra'
 // import helmet from 'helmet'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
@@ -7,19 +7,21 @@ import { AppModule } from './app.module'
 async function bootstrap() {
    const PORT = process.env.PORT || 8000
 
-   const httpsOptions = {
-      key: fs.readFileSync('secret/privkey.pem'),
-      cert: fs.readFileSync('secret/cert.pem')
-   }
+   // const httpsOptions = {
+   //    key: fs.readFileSync('secret/privkey.pem'),
+   //    cert: fs.readFileSync('secret/cert.pem')
+   // }
 
    const graphqlUploadExpress = (await import('graphql-upload/graphqlUploadExpress.mjs')).default
 
-   const app = await NestFactory.create<NestApplication>(AppModule, { httpsOptions })
+   // const app = await NestFactory.create<NestApplication>(AppModule, { httpsOptions })
+   const app = await NestFactory.create<NestApplication>(AppModule)
 
    app.use(cookieParser())
    app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }))
 
    const corsOptions = {
+      // origin: 'https://localhost:3000',
       origin: 'https://app.navalista.com',
       credentials: true,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
